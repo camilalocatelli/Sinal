83954c7f9455d9024cfc8e25f9135bd43c511b0c
camilalocatelli <locatelli.camila6@gmail.com>
2022-04-01 22:18:07 -0300

    Finalizado!

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿int numero;
+Console.Write("Digite um número: ");
+numero = Convert.ToInt32 (Console.ReadLine());
+
+if (numero < 0)
+{
+    Console.ForegroundColor = ConsoleColor.DarkRed;
+    Console.WriteLine("Negativo!");
+}
+else if (numero > 0)
+{
+    Console.ForegroundColor = ConsoleColor.DarkGreen;
+    Console.WriteLine("Positivo!");
+}
+else
+{
+    Console.ForegroundColor = ConsoleColor.Yellow;
+    Console.WriteLine("Zero!");
+}
+
+Console.ResetColor();
